da7d8766a09f45c95a3da9daaf4773495201a5cb
marpilip <mar.pilip@gmail.com>
2023-07-26 22:58:26 +0700

    added board, player, moveHistory

--- src/main/java/io/deeplay/model/player/Bot.java
+++ src/main/java/io/deeplay/model/player/Bot.java
@@ -1,2 +1,32 @@
-package io.deeplay.model;public class Bot {
+package io.deeplay.model.player;
+
+public class Bot extends Player {
+    private int difficultyLevel;
+
+    public Bot(char piecesColor, int difficultyLevel) {
+        super(piecesColor);
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    @Override
+    public void move() {
+        // random move
+    }
+
+    public int getDifficultyLevel() {
+        return difficultyLevel;
+    }
+
+    public void chooseDifficultyLevel(int level) {
+        switch (level) {
+            case 1: // easy
+                break;
+            case 2: //medium
+                break;
+            case 3: //hard
+                break;
+            default: // incorrect input
+                break;
+        }
+    }
 }
