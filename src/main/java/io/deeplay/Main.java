711953c9f5de2b292a633de51d45cf6f36d8614c
RealCryZed <bujhmgtnhjd@bk.ru>
2023-07-28 12:39:15 +0600

    refactor: moved starting game logic from Main class to SessionService class, removed method startGame() from SelfPlay

--- src/main/java/io/deeplay/Main.java
+++ src/main/java/io/deeplay/Main.java
@@ -2,12 +2,7 @@ package io.deeplay;
 
 import io.deeplay.engine.SelfPlay;
 import io.deeplay.model.Board;
-import io.deeplay.model.player.Bot;
-import io.deeplay.model.player.Human;
-import io.deeplay.service.UserCommunicationService;
-
-import java.nio.charset.Charset;
-import java.util.Scanner;
+import io.deeplay.service.SessionService;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,32 +17,7 @@ public class Main {
 //        System.out.println();
 //        board.printBoard();
 
-        // С начальной страницы получить тип игры,
-
-        System.out.println("Game opened...");
-
-        Scanner scanner = new Scanner(System.in, Charset.defaultCharset().name());
-
-        // Временно выбор всегда консоли
-        char inputType = 'c';
-
-        System.out.println("choose type of the game (bot-bot / human-human / human-bot)");
-        String gameType = scanner.nextLine();
-
-        if (gameType.equals("bot-bot")) {
-            SelfPlay selfPlay = new SelfPlay(new Bot('w', UserCommunicationService.chooseBotLevel()), new Bot('b', UserCommunicationService.chooseBotLevel()));
-            selfPlay.startGame();
-        } else if (gameType.equals("human-human")) {
-            char[] userColor = UserCommunicationService.chooseColor();
-            SelfPlay selfPlay = new SelfPlay(new Human(userColor[0]), new Human(userColor[1]));
-            selfPlay.startGame();
-        } else if (gameType.equals("human-bot")) {
-            char[] userColor = UserCommunicationService.chooseColor();
-            SelfPlay selfPlay = new SelfPlay(new Human(userColor[0]), new Bot(userColor[1], UserCommunicationService.chooseBotLevel()));
-            selfPlay.startGame();
-        } else {
-            System.out.println("Invalid input. Ending...");
-            System.exit(0);
-        }
+        SelfPlay selfPlay = SessionService.openGameAndGetInfo();
+        SessionService.startGameSession(selfPlay);
     }
 }
