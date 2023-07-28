d1b5588907482f40d0173c1cfac80aed16359f0b
RealCryZed <bujhmgtnhjd@bk.ru>
2023-07-28 16:48:39 +0600

    fix: fixed bug with packages

--- src/main/java/io/deeplay/service/PieceService.java
+++ src/main/java/io/deeplay/service/PieceService.java
@@ -1,6 +1,8 @@
 package io.deeplay.service;
 
-import io.deeplay.model.Piece;
+import io.deeplay.model.piece.Color;
+import io.deeplay.model.piece.Piece;
+import io.deeplay.model.piece.Rook;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,6 +13,6 @@ public class PieceService {
     public static List<Piece> getPossibleToMovePieces() {
         // вернуть List с фигурами
 
-        return new ArrayList<>(List.of(new Piece()));
+        return new ArrayList<>(List.of(new Rook(0, 0, Color.WHITE)));
     }
 }
