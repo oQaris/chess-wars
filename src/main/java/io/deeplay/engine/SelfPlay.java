9308069efe376c66a9467d5e7ff2caf4d79d0d23
RealCryZed <bujhmgtnhjd@bk.ru>
2023-07-28 13:10:37 +0600

    refactor: removed SessionService and created GameSession class instead

--- src/main/java/io/deeplay/engine/SelfPlay.java
+++ src/main/java/io/deeplay/engine/SelfPlay.java
@@ -4,7 +4,6 @@ import io.deeplay.domain.GameType;
 import io.deeplay.model.player.Bot;
 import io.deeplay.model.player.Human;
 import io.deeplay.model.player.Player;
-import io.deeplay.service.SessionService;
 
 // Класс должен иметь одну цель - самостоятельно понимать кто ходит,
 // а также записывать в лог каждый ход
@@ -14,20 +13,14 @@ public class SelfPlay {
     private Player player2;
     private GameType gameType;
 
-    public SelfPlay(Player player1, Player player2) {
+    public SelfPlay(Player player1, Player player2, GameType gameType) {
         this.player1 = player1;
         this.player2 = player2;
+        this.gameType = gameType;
     }
 
     // метод выбирает, кто ходит в данный момент
     public void chooseMove() {
 
-    }
-
-    private GameType checkGameType() {
-        System.out.println("checking game type...");
-        if (player1 instanceof Bot && player2 instanceof Bot) return GameType.BotVsBot;
-        else if (player1 instanceof Human && player2 instanceof Human) return GameType.HumanVsHuman;
-        else return GameType.HumanVsBot;
     }
 }
