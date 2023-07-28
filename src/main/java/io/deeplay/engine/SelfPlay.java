cc88fd639c9c28b580980a91f2f59d705627a546
RealCryZed <bujhmgtnhjd@bk.ru>
2023-07-28 12:50:51 +0600

    refactor: moved enum GameType from SelfPlay class to domain package, added method chooseMove() in SelfPlay

--- src/main/java/io/deeplay/engine/SelfPlay.java
+++ src/main/java/io/deeplay/engine/SelfPlay.java
@@ -1,5 +1,6 @@
 package io.deeplay.engine;
 
+import io.deeplay.domain.GameType;
 import io.deeplay.model.player.Bot;
 import io.deeplay.model.player.Human;
 import io.deeplay.model.player.Player;
@@ -11,16 +12,16 @@ public class SelfPlay {
 
     private Player player1;
     private Player player2;
-
-    enum GameType {
-        BotVsBot,
-        HumanVsBot,
-        HumanVsHuman
-    }
+    private GameType gameType;
 
     public SelfPlay(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
+    }
+
+    // метод выбирает, кто ходит в данный момент
+    public void chooseMove() {
+
     }
 
     private GameType checkGameType() {
