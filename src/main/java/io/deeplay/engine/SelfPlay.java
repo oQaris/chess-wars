528ab9abe62983f91841a56becb2e6f50ee2e060
RealCryZed <bujhmgtnhjd@bk.ru>
2023-07-28 12:25:05 +0600

    refactor: moved some service methods to new classes: SessionService and UserCommunicationService

--- src/main/java/io/deeplay/engine/SelfPlay.java
+++ src/main/java/io/deeplay/engine/SelfPlay.java
@@ -3,9 +3,7 @@ package io.deeplay.engine;
 import io.deeplay.model.player.Bot;
 import io.deeplay.model.player.Human;
 import io.deeplay.model.player.Player;
-
-import java.nio.charset.Charset;
-import java.util.Scanner;
+import io.deeplay.service.SessionService;
 
 public class SelfPlay {
 
@@ -25,36 +23,7 @@ public class SelfPlay {
 
     public void startGame() {
         System.out.println("Game started!");
-    }
-
-    public void startGameSession() {
-
-    }
-
-    public static char[] chooseColor() {
-        Scanner scanner = new Scanner(System.in, Charset.defaultCharset().name());
-        char[] userInput = new char[2];
-        while (true) {
-            System.out.println("choose color of player1");
-            userInput[0] = scanner.next().charAt(0);
-            if (userInput[0] == 'w') {
-                System.out.println("player1 color is set to white");
-                System.out.println("player2 color is set to black");
-                userInput[1] = 'b';
-                return userInput;
-            } else if (userInput[0] == 'b'){
-                System.out.println("player1 color is set to black");
-                System.out.println("player2 color is set to white");
-                userInput[1] = 'w';
-                return userInput;
-            }
-        }
-    }
-
-    public static int chooseBotLevel() {
-        // Scanner или с помощью интерфейса вводить число от 1 до 3
-
-        return 1;
+        SessionService.startGameSession();
     }
 
     private GameType checkGameType() {
