8c9d2b4e28349ce37e6e67fba0aff84433cf9771
RealCryZed <bujhmgtnhjd@bk.ru>
2023-07-28 13:10:37 +0600

    refactor: removed SessionService and created GameSession class instead

--- src/main/java/io/deeplay/service/UserCommunicationService.java
+++ src/main/java/io/deeplay/service/UserCommunicationService.java
@@ -1,5 +1,10 @@
 package io.deeplay.service;
 
+import io.deeplay.domain.GameType;
+import io.deeplay.engine.SelfPlay;
+import io.deeplay.model.player.Bot;
+import io.deeplay.model.player.Human;
+
 import java.nio.charset.Charset;
 import java.util.Scanner;
 
@@ -29,5 +34,30 @@ public class UserCommunicationService {
                 return userInput;
             }
         }
+    }
+
+    public static SelfPlay getGameInfo() {
+        // С начальной страницы получить тип игры
+        System.out.println("Game opened...");
+        Scanner scanner = new Scanner(System.in, Charset.defaultCharset().name());
+
+        char inputType = 'c';
+
+        System.out.println("choose type of the game (bot-bot / human-human / human-bot)");
+        String gameType = scanner.nextLine();
+
+        if (gameType.equals("bot-bot")) {
+            return new SelfPlay(new Bot('w', UserCommunicationService.chooseBotLevel()), new Bot('b', UserCommunicationService.chooseBotLevel()), GameType.BotVsBot);
+        } else if (gameType.equals("human-human")) {
+            char[] userColor = UserCommunicationService.chooseColor();
+            return new SelfPlay(new Human(userColor[0]), new Human(userColor[1]), GameType.HumanVsHuman);
+        } else if (gameType.equals("human-bot")) {
+            char[] userColor = UserCommunicationService.chooseColor();
+            return new SelfPlay(new Human(userColor[0]), new Bot(userColor[1], UserCommunicationService.chooseBotLevel()), GameType.HumanVsBot);
+        } else {
+            System.out.println("Invalid input. Ending...");
+            System.exit(0);
+        }
+        return null;
     }
 }
