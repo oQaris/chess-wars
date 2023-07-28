129fe9f69d471cc41cb4a2add508e94a06f7d761
RealCryZed <bujhmgtnhjd@bk.ru>
2023-07-28 11:39:29 +0600

    feat: added class SelfPlay for managing game session and switching moves between players. Added 'piecesColor' class field and toString() method for Bot.

--- src/main/java/io/deeplay/Main.java
+++ src/main/java/io/deeplay/Main.java
@@ -1,18 +1,52 @@
 package io.deeplay;
 
+import io.deeplay.engine.SelfPlay;
 import io.deeplay.model.Board;
+import io.deeplay.model.player.Bot;
+import io.deeplay.model.player.Human;
+
+import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+//
+//        Board board = new Board();
+//
+//        board.printBoard();
+//        board.movePiece(48, 40);
+//
+//        System.out.println();
+//        System.out.println();
+//        board.printBoard();
 
-        Board board = new Board();
+        // С начальной страницы получить тип игры,
 
-        board.printBoard();
-        board.movePiece(48, 40);
+        System.out.println("Game opened...");
 
-        System.out.println();
-        System.out.println();
-        board.printBoard();
+        Scanner scanner = new Scanner(System.in, Charset.defaultCharset().name());
+
+        // Временно выбор всегда консоли
+        char inputType = 'c';
+
+        System.out.println("choose type of the game (bot-bot / human-human / human-bot)");
+        String gameType = scanner.nextLine();
+
+        if (gameType.equals("bot-bot")) {
+            SelfPlay selfPlay = new SelfPlay(new Bot('w', SelfPlay.chooseBotLevel()), new Bot('b', SelfPlay.chooseBotLevel()));
+            selfPlay.startGame();
+        } else if (gameType.equals("human-human")) {
+            char[] userColor = SelfPlay.chooseColor();
+            SelfPlay selfPlay = new SelfPlay(new Human(userColor[0]), new Human(userColor[1]));
+            selfPlay.startGame();
+        } else if (gameType.equals("human-bot")) {
+            char[] userColor = SelfPlay.chooseColor();
+            SelfPlay selfPlay = new SelfPlay(new Human(userColor[0]), new Bot(userColor[1], SelfPlay.chooseBotLevel()));
+            selfPlay.startGame();
+        } else {
+            System.out.println("Invalid input. Ending...");
+            System.exit(0);
+        }
     }
 }
