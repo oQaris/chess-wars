b28ebbe1e95a8f94119702481b1524ae23ac587e
marpilip <mar.pilip@gmail.com>
2023-07-31 13:43:28 +0700

    update

---
+++ src/main/java/io/deeplay/service/PieceService.java
@@ -0,0 +1,10 @@
+package io.deeplay.service;
+
+import io.deeplay.model.Board;
+import io.deeplay.model.piece.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PieceService {
+}
