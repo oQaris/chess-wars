e3ba71bf135273dda743a5208a142aff7ee26378
pai9maker <rapolovgenrik@gmail.com>
2023-07-31 11:06:49 +0300

    Pieces move system

---
+++ src/main/java/io/deeplay/model/piece/Empty.java
@@ -0,0 +1,29 @@
+package io.deeplay.model.piece;
+
+import io.deeplay.model.Board;
+import io.deeplay.model.Coordinates;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Empty extends Piece {
+    public Empty(Coordinates coordinates, Color color) {
+        super(coordinates, Color.EMPTY);
+    }
+
+    @Override
+    public List<Coordinates> getPossibleMoves(Board board) {
+        return null;
+    }
+
+    @Override
+    public boolean canMoveAt(Coordinates coordinates, Board board) {
+        return false;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Empty: x = " + getCoordinates().getX() + " y = " + getCoordinates().getY();
+    }
+}
