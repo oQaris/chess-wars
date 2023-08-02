1d0a177ea051443f09e72d7de105e325d8028ab3
RealCryZed <bujhmgtnhjd@bk.ru>
2023-08-02 15:49:24 +0600

    Added tests in BotTest

--- src/test/java/io/deeplay/model/player/BotTest.java
+++ src/test/java/io/deeplay/model/player/BotTest.java
@@ -1,7 +1,13 @@
 package io.deeplay.model.player;
 
+import io.deeplay.domain.Color;
+import io.deeplay.model.Board;
+import io.deeplay.model.Coordinates;
 import io.deeplay.model.move.Move;
 import io.deeplay.model.move.MoveType;
+import io.deeplay.model.piece.Knight;
+import io.deeplay.model.piece.Piece;
+import io.deeplay.service.UserCommunicationService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,20 +17,33 @@ import java.util.List;
 
 class BotTest {
 
-    private List<Move> possibleMoves;
+    private List<Piece> possiblePiecesToMove;
+    private Board board;
 
     @BeforeEach
-    public void initializeBoard() {
-        possibleMoves = new ArrayList<>();
-        //     possibleMoves.add(new Move(8, 16, MoveType.ORDINARY, '\0'));
-        //     possibleMoves.add(new Move(9, 17, MoveType.ORDINARY, '\0'));
-        //     possibleMoves.add(new Move(10, 18, MoveType.ORDINARY, '\0'));
-        //     possibleMoves.add(new Move(11, 19, MoveType.ORDINARY, '\0'));
-        //     possibleMoves.add(new Move(12, 20, MoveType.ORDINARY, '\0'));
+    public void initialize() {
+        board = new Board();
+        possiblePiecesToMove = new ArrayList<>();
+        possiblePiecesToMove.add(new Knight(new Coordinates(6, 0), Color.WHITE));
+        possiblePiecesToMove.add(new Knight(new Coordinates(1, 0), Color.WHITE));
     }
 
     @Test
     void testRandomMove() {
+        final Bot bot = new Bot(Color.WHITE, 1);
+        Move result = bot.getMove(possiblePiecesToMove, board);
 
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void testSingleAvailableMove() {
+        final Bot bot = new Bot(Color.WHITE, 1);
+        possiblePiecesToMove.remove(1);
+
+        Assertions.assertDoesNotThrow(() -> bot.getMove(possiblePiecesToMove, board));
+
+        Move result = bot.getMove(possiblePiecesToMove, board);
+        Assertions.assertNotNull(result);
     }
 }
