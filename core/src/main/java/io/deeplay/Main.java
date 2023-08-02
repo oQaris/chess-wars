1bf85ccca36bd66279d76d6d7c58477aafdccbeb
pai9maker <rapolovgenrik@gmail.com>
2023-08-02 09:04:12 +0300

    hotfix of pawn

--- core/src/main/java/io/deeplay/Main.java
+++ core/src/main/java/io/deeplay/Main.java
@@ -2,6 +2,9 @@ package io.deeplay;
 
 import io.deeplay.engine.GameSession;
 import io.deeplay.model.Board;
+import io.deeplay.model.Coordinates;
+import io.deeplay.model.piece.Piece;
+import io.deeplay.model.utils.BoardUtils;
 import io.deeplay.service.UserCommunicationService;
 
 public class Main {
@@ -9,6 +12,7 @@ public class Main {
 //        System.out.println("Hello world!");
 //
         Board board = new Board();
+        BoardUtils utils = new BoardUtils();
 //
 //        board.printBoard();
 //        board.movePiece(48, 40);
@@ -16,8 +20,13 @@ public class Main {
 //        System.out.println();
 //        System.out.println();
 //        board.printBoard();
+        for (int i = 0; i < 8; i ++) {
+            for (int j = 0; j < 8; j ++){
+                utils.render(board, board.getPiece(new Coordinates(i,j)));
+            }
+        }
 
-        GameSession gameSession = UserCommunicationService.getGameSessionInfo();
-        gameSession.startGameSession();
+//        GameSession gameSession = UserCommunicationService.getGameSessionInfo();
+//        gameSession.startGameSession();
     }
 }
