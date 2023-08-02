a8d6c0ae1da34c4cd83cb104f1f3a1d61a13bc45
pai9maker <rapolovgenrik@gmail.com>
2023-08-02 09:04:12 +0300

    hotfix of pawn

--- core/src/main/java/io/deeplay/model/piece/Pawn.java
+++ core/src/main/java/io/deeplay/model/piece/Pawn.java
@@ -21,20 +21,11 @@ public class Pawn extends Piece{
     public List<Coordinates> getPossibleMoves(Board board) {
         List<Coordinates> possibleMoves = new ArrayList<>();
 
-        int[] dx = {-1, 1, -1, 1};
-        int[] dy = {-1, -1, 1, 1};
-
-        for (int i = 0; i < dx.length; i++) {
-            int currentX = getCoordinates().getX() + dx[i];
-            int currentY = getCoordinates().getY() + dy[i];
-
-            while (currentX >= 0 && currentX < 8 && currentY >= 0 && currentY < 8) {
-                Coordinates coordinates = new Coordinates(currentX, currentY);
-                if (canMoveAt(coordinates, board)) {
-                    possibleMoves.add(coordinates);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (canMoveAt(new Coordinates(i, j), board)) {
+                    possibleMoves.add(new Coordinates(i, j));
                 }
-                currentX += dx[i];
-                currentY += dy[i];
             }
         }
         return possibleMoves;
@@ -42,29 +33,45 @@ public class Pawn extends Piece{
 
     @Override
     public boolean canMoveAt(Coordinates coordinates, Board board) {
-        int dx = coordinates.getX() - getCoordinates().getX();
-        int dy = coordinates.getY() - getCoordinates().getY();
-
-        if (Math.abs(dx) != Math.abs(dy)) {
+        if (coordinates.getX() < 0 || coordinates.getY() < 0 || coordinates.getY() >= 8 || coordinates.getX() >= 8) {
+            return false;
+        }
+        if (this.getCoordinates().getX() == coordinates.getX() && this.getCoordinates().getY() == coordinates.getY()) {
+            return false;
+        }
+        if (board.getBoard()[coordinates.getX()][coordinates.getY()].getColor().equals(getColor())) { // фигура того же цвета
+            return false;
+        }
+        if (Math.abs(coordinates.getX() - this.getCoordinates().getX()) == Math.abs(coordinates.getY() - this.getCoordinates().getY())) {
             return false;
         }
 
-        int xDirection = Integer.compare(coordinates.getX(), getCoordinates().getX());
-        int yDirection = Integer.compare(coordinates.getY(), getCoordinates().getY());
+        int xDirection = Integer.compare(coordinates.getX(), this.getCoordinates().getX());
+        int yDirection = Integer.compare(coordinates.getY(), this.getCoordinates().getY());
 
-        int currentX = getCoordinates().getX() + xDirection;
-        int currentY = getCoordinates().getY() + yDirection;
+        int currentX = this.getCoordinates().getX() + xDirection;
+        int currentY = this.getCoordinates().getY() + yDirection;
 
-        while (currentX != coordinates.getX() || currentY != coordinates.getY()) {
-            if (board.getPiece(new Coordinates(currentX, currentY)).equals(Color.EMPTY)) {
-                return false;
-            }
-
-            currentX += xDirection;
-            currentY += yDirection;
+        int distance = Math.abs(coordinates.getY() - this.getCoordinates().getY());
+        if (!board.getBoard()[currentX][currentY].getColor().equals(Color.EMPTY)) { // если не пустая, на пути стоит фигура другого цвета
+            return false;
         }
-
-        return true;
+        if ((coordinates.getY() == 1 && board.getPiece(coordinates).getColor() == Color.WHITE) || (coordinates.getY() == 6 && board.getPiece(coordinates).getColor() == Color.BLACK)) {
+            if (distance == 2) {
+                return true;
+            }
+        }
+        if (this.getCoordinates().getX() == coordinates.getX() && this.getCoordinates().getY() + yDirection == coordinates.getY() && board.getPiece(coordinates).getColor().equals(Color.EMPTY) && distance == 1) {
+            return true;
+        }
+        if (Math.abs(this.getCoordinates().getX() - coordinates.getX()) == 1 && this.getCoordinates().getY() + yDirection == coordinates.getY()) {
+            if (!board.getBoard()[coordinates.getX()][coordinates.getY()].getColor().equals(Color.EMPTY)) {
+                return true;
+            }
+        }
+        currentX += xDirection;
+        currentY += yDirection;
+        return false;
     }
 
     @Override
--- core/src/main/java/io/deeplay/model/utils/BoardUtils.java
+++ core/src/main/java/io/deeplay/model/utils/BoardUtils.java
@@ -3,48 +3,67 @@ package io.deeplay.model.utils;
 import io.deeplay.domain.Color;
 import io.deeplay.model.Board;
 import io.deeplay.model.Coordinates;
-import io.deeplay.model.piece.Piece;
+import io.deeplay.model.piece.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
-import static io.deeplay.model.Board.*;
 
 
 public class BoardUtils {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_WHITE_PIECE_COLOR = "\u001B[97m";
     public static final String ANSI_BLACK_PIECE_COLOR = "\u001B[30m";
-
     public static final String ANSI_WHITE_SQUARE_BACKGROUND = "\u001B[47m";
-
     public static final String ANSI_BLACK_SQUARE_BACKGROUND = "\u001B[0;100m";
-
     public static final String ANSI_HIGHLIGHTED_SQUARE_BACKGROUND = "\u001B[45m";
 
     public void render(Board board, Piece pieceToMove) {
-            List<Coordinates> availableMoveSquares = new ArrayList<>();
+        List<Coordinates> availableMoveSquares = new ArrayList<>();
+        switch(pieceToMove.getClass().getSimpleName()){
+            case "Pawn":
+                pieceToMove = new Pawn(pieceToMove.getCoordinates(), pieceToMove.getColor());
+                break;
+            case "Knight":
+                pieceToMove = new Knight(pieceToMove.getCoordinates(), pieceToMove.getColor());
+                break;
+            case "Bishop":
+                pieceToMove = new Bishop(pieceToMove.getCoordinates(), pieceToMove.getColor());
+                break;
+            case "Rook":
+                pieceToMove = new Rook(pieceToMove.getCoordinates(), pieceToMove.getColor());
+                break;
+            case "Queen":
+                pieceToMove = new Queen(pieceToMove.getCoordinates(), pieceToMove.getColor());
+                break;
+            case "King":
+                pieceToMove = new King(pieceToMove.getCoordinates(), pieceToMove.getColor());
+                break;
+            case "Empty":
+                pieceToMove = new Empty(pieceToMove.getCoordinates(), pieceToMove.getColor());
+                break;
+        }
+
         if (pieceToMove != null) {
-            availableMoveSquares = Piece.getPossibleMoves(board);
+            availableMoveSquares.addAll(pieceToMove.getPossibleMoves(board));
         }
 
         for (int i = 7; i >= 0; i--) {
             String line = "";
-            for (int j =0; j < 8; j++) {
+            for (int j = 0; j < 8; j++) {
                 assert availableMoveSquares != null;
                 boolean isHighlight = availableMoveSquares.contains(new Coordinates(i, j));
-
-                if (board.isSquareEmpty(new Coordinates(i, j))) {
-                    line += getSpriteForEmptySquare(new Coordinates(i,j), isHighlight);
+                if (isSquareEmpty(board, new Coordinates(i, j))) {
+                    line += "   " + getSpriteForEmptySquare(new Coordinates(i,j), isHighlight);
                 } else {
                     line += getPieceSprite(board.getPiece(new Coordinates(i,j)), isHighlight);
                 }
             }
-
             line += ANSI_RESET;
             System.out.println(line);
         }
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("------------------------------------------------------------------------");
     }
 
     public void render(Board board) {
@@ -54,13 +73,11 @@ public class BoardUtils {
     private static String colorizeSprite(String sprite, Color pieceColor, boolean isSquareDark, boolean isHighlight) {
         // format = background color + font color + text
         String result = sprite;
-
         if (pieceColor == Color.WHITE) {
             result = ANSI_WHITE_PIECE_COLOR + result;
         } else {
             result = ANSI_BLACK_PIECE_COLOR + result;
         }
-
         if (isHighlight) {
             result = ANSI_HIGHLIGHTED_SQUARE_BACKGROUND + result;
         } else if (isSquareDark) {
@@ -68,21 +85,24 @@ public class BoardUtils {
         } else {
             result = ANSI_WHITE_SQUARE_BACKGROUND + result;
         }
-
         return result;
     }
 
     private String getSpriteForEmptySquare(Coordinates coordinates, boolean isHighlight) {
-        return colorizeSprite("   ", Color.WHITE, isSquareDark(coordinates), isHighlight);
+        return colorizeSprite("  ", Color.WHITE, isSquareDark(coordinates), isHighlight);
     }
 
-    private boolean isSquareDark(Coordinates coordinates) {
+    private static boolean isSquareDark(Coordinates coordinates) {
+        return (coordinates.getX() + coordinates.getY()) % 2 == 1;
+    }
 
+    private boolean isSquareEmpty(Board board, Coordinates coordinates) {
+        return board.getPiece(coordinates) == null;
     }
 
     private static String selectUnicodeSpriteForPiece(Piece piece) {
         return switch (piece.getClass().getSimpleName()) {
-            case "Pawn" -> "♟︎";
+            case "Pawn" -> "♟";
             case "Knight" -> "♞";
             case "Bishop" -> "♝";
             case "Rook" -> "♜";
@@ -90,21 +110,12 @@ public class BoardUtils {
             case "King" -> "♚";
             default -> "";
         };
-
     }
 
     private static String getPieceSprite(Piece piece, boolean isHighlight) {
         return colorizeSprite(
-                "   " + selectUnicodeSpriteForPiece(piece) + "   ", piece.getColor(), Board.isSquareDark(piece.getCoordinates()),
+                " " + selectUnicodeSpriteForPiece(piece) + "  ", piece.getColor(), isSquareDark(piece.getCoordinates()),
                 isHighlight
         );
-    }
-
-    public static void main(String[] args) {
-        for (int i = 0; i < 7; i ++) {
-            String line = "";
-            for (int j =0; j < 7; j ++){
-            }
-        }
     }
 }
