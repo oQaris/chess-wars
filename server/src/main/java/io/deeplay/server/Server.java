3879f723aecd713454a46d882ce5f154c96767d8
marpilip <mar.pilip@gmail.com>
2023-08-09 14:18:38 +0700

    fix: MoveHistory rejected static, pawn, deleted ServerHandler class

--- server/src/main/java/io/deeplay/server/Server.java
+++ server/src/main/java/io/deeplay/server/Server.java
@@ -1,27 +1,5 @@
 package io.deeplay.server;
 
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-
 public class Server {
-    private int port;
-
-    public Server(int port) {
-        this.port = port;
-    }
-
-    public static void main(String[] args) {
-        int port = 8888;
-        Server server = new Server(port);
-        try {
-            server.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void start() {
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-    }
+    int port;
 }
