b30f83c2e721060e7b8e25465168b7c6c555fb47
RealCryZed <bujhmgtnhjd@bk.ru>
2023-08-10 17:23:22 +0600

    minor changes

--- communication/src/main/java/service/SerializationService.java
+++ communication/src/main/java/service/SerializationService.java
@@ -1,10 +1,6 @@
 package service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
-import dto.MoveDTO;
-
-import java.io.IOException;
 
 public class SerializationService {
     private static final Gson gson = new Gson();
