21405b9bfb3a1180b4563727e2f6451c3f25e032
marpilip <mar.pilip@gmail.com>
2023-08-11 14:48:31 +0700

    add: documentation for pieces, deleted some useless methods

--- core/src/main/java/io/deeplay/domain/Color.java
+++ core/src/main/java/io/deeplay/domain/Color.java
@@ -3,14 +3,5 @@ package io.deeplay.domain;
 public enum Color {
     WHITE,
     BLACK,
-    EMPTY;
-
-    public Color opposite() {
-        if (this == WHITE) {
-            return BLACK;
-        } else if (this == BLACK) {
-            return WHITE;
-        }
-        return EMPTY;
-    }
+    EMPTY
 }
