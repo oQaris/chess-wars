11adccfebc4fd06527f4e02100307d85eaca2a65
marpilip <mar.pilip@gmail.com>
2023-08-17 00:42:01 +0700

    feat: Server Board startGame()

--- server/src/main/java/io/deeplay/server/ClientHandler.java
+++ server/src/main/java/io/deeplay/server/ClientHandler.java
@@ -2,32 +2,32 @@ package io.deeplay.server;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.deeplay.domain.Color;
 import io.deeplay.engine.GameSession;
 import io.deeplay.model.Board;
 import io.deeplay.model.move.Move;
-import io.deeplay.model.move.MoveHistory;
-import io.deeplay.service.UserCommunicationService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
+    private static final Logger logger = LogManager.getLogger(ClientHandler.class);
     Socket clientSocket;
-    private Server server;
+    private final Server server;
     BufferedWriter out;
     BufferedReader in;
     private ObjectMapper objectMapper;
     private String player;
     private GameSession gameSession;
-    private Board board;
-    private MoveHistory moveHistory;
+    Board board;
 
     public ClientHandler(Socket clientSocket, Server server) {
         this.clientSocket = clientSocket;
         this.server = server;
         objectMapper = new ObjectMapper();
         board = new Board();
-        moveHistory = new MoveHistory();
     }
 
     @Override
@@ -37,28 +37,24 @@ public class ClientHandler implements Runnable {
             out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            UserCommunicationService userCommunicationService = new UserCommunicationService(System.in, System.out);
-            GameSession gameSession = userCommunicationService.getGameSessionInfo();
-            gameSession.startGameSession();
-
             player = getPlayerName();
             startGame();
 
             server.broadcast("Player " + player + " had connected");
-
 
             // ожидание ходов от клиента
             while ((clientInput = in.readLine()) != null) {
                 handleClientInput(clientInput);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Ошибка при подключении игрока: ", e);
         } finally {
             try {
                 in.close();
                 out.close();
                 clientSocket.close();
             } catch (IOException e) {
+                logger.error("Ошибка при прочтении данных: ", e);
                 e.printStackTrace();
             }
         }
@@ -94,25 +90,45 @@ public class ClientHandler implements Runnable {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            logger.error("Ошибка: ", e);
         }
     }
 
-    private String getPlayerName() throws IOException {
+    private String getPlayerName(){
         sendMessageToClient("Enter your name:");
-        return in.readLine();
+        String name = "";
+
+        try {
+            return in.readLine();
+        } catch (IOException e) {
+            logger.error("Игрок не ввел имя", e);
+        }
+
+        return name;
     }
 
-    public void sendMessage(String message) throws IOException {
-        out.write(message);
-        out.flush();
-        out.newLine();
+    public void sendMessage(String message) {
+        try {
+            out.write(message);
+            out.flush();
+            out.newLine();
+        } catch (IOException e) {
+            logger.error("Не получилось отправить сообщение ", e);
+        }
     }
 
-    public void sendMove(String move) throws IOException {
-        String jsonData = objectMapper.writeValueAsString(move);
-        out.write(jsonData);
-        out.flush();
+    public void sendMove(String move) {
+        String jsonData;
+
+        try {
+            jsonData = objectMapper.writeValueAsString(move);
+            out.write(jsonData);
+            out.flush();
+        } catch (JsonProcessingException e) {
+            logger.error("Не получилось преобразовать данные ", e);
+        } catch (IOException e) {
+            logger.error("Не получилось отправить данные на сервер ", e);
+        }
     }
 
     private void sendMoveToServer(String move) throws IOException {
@@ -124,35 +140,72 @@ public class ClientHandler implements Runnable {
         try {
             return in.readLine();
         } catch (IOException e) {
+            logger.error("Не получилось обработать ответ сервера: ", e);
             e.printStackTrace();
             return null;
         }
     }
 
-    void sendMessageToClient(String message) throws IOException {
-        out.write(message);
-        out.flush();
+    /**
+     * Отправляет сообщение клиенту.
+     *
+     * @param message сообщение для отправки
+     */
+    void sendMessageToClient(String message) {
+        try {
+            out.write(message);
+            out.flush();
+        } catch (IOException e) {
+            logger.error("Не получилось отправить сообщение клиенту: ", e);
+            e.printStackTrace();
+        }
     }
 
-    private boolean validateMove(String moveString) throws JsonProcessingException {
-        Move move = objectMapper.readValue(moveString, Move.class);
+    /**
+     * Проверяет корректность хода.
+     *
+     * @param moveString строка с ходом
+     * @return true, если ход корректный, false в противном случае
+     * * @throws JsonProcessingException
+     */
+    private boolean validateMove(String moveString) {
+        Move move = null;
+        try {
+            move = objectMapper.readValue(moveString, Move.class);
+        } catch (JsonProcessingException e) {
+            logger.error("Не получилось обработать ход: ", e);
+            e.printStackTrace();
+        }
 
         // Получение текущего состояния доски
         Board board = server.getBoardState();
 
         // Проверка корректности хода
-        return board.getPiece(move.getStartPosition()).canMoveAt(move.getEndPosition(), board);
+        return board.getPiece(move.startPosition()).canMoveAt(move.endPosition(), board);
     }
 
-    private void startGame() throws IOException {
+    private synchronized void startGame() throws IOException {
         sendMessageToClient("Are you ready to start the game? (yes/no)");
         String response = in.readLine();
         if (response.equalsIgnoreCase("yes")) {
-            // server.startGame(player);
-            sendMessageToClient("The game has started!");
+            // server.startGame();
+            sendMessageToClient("Welcome to the chess");
         } else {
             sendMessageToClient("Okay, maybe next time.");
+            logger.info("Игрок не согласился на игру");
             throw new RuntimeException("Player " + player + " declined to start the game.");
         }
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Color getPlayerColor() {
+        return Color.EMPTY;
+    }
+
+    public Move receiveMoveFromClient() {
+        return null;
     }
 }
--- server/src/main/java/io/deeplay/server/Server.java
+++ server/src/main/java/io/deeplay/server/Server.java
@@ -1,11 +1,17 @@
 package io.deeplay.server;
 
+import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dto.MoveDTO;
+import io.deeplay.communication.dto.MoveDTO;
+import io.deeplay.communication.service.SerializationService;
+import io.deeplay.domain.Color;
 import io.deeplay.engine.GameSession;
+import io.deeplay.engine.GameState;
 import io.deeplay.model.Board;
-import io.deeplay.service.UserCommunicationService;
-import service.SerializationService;
+import io.deeplay.model.move.Move;
+import io.deeplay.model.move.MoveHistory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -15,54 +21,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
+    private static final Logger logger = LogManager.getLogger(Server.class);
     private static final int PORT = 8080;
     private ServerSocket serverSocket;
     private ObjectMapper mapper;
     private List<ClientHandler> clients = new ArrayList<>();
+    private int currentPlayer = 0;
     private GameSession gameSession;
     private boolean isGameStarted;
-    private boolean gameStarted;
+    private boolean gameStarted = false;
+    private DeserializationContext objectMapper;
 
-    public Server() {
+    public Server(int port) {
         mapper = new ObjectMapper();
     }
 
     public static void main(String[] args) throws IOException {
         System.setOut(new java.io.PrintStream(System.out, true, StandardCharsets.UTF_8));
-        Server server = new Server();
+        Server server = new Server(PORT);
         server.start(2);
     }
 
     public void start(int playersNumber) throws IOException {
         try {
             serverSocket = new ServerSocket(PORT);
-            System.out.println("Server started on port " + PORT);
+            logger.info("Server started on port " + PORT);
         } catch (IOException e) {
-            System.err.println("Could not listen on port: " + PORT);
+            logger.error("Could not listen on port: " + PORT);
             System.exit(1);
         }
 
-        System.out.println("Ready to accept");
+        logger.info("Ready to accept");
 
         while (clients.size() < playersNumber) {
             Socket socket = serverSocket.accept();
-            System.out.println("Client " + (clients.size() + 1) + " connected");
+            logger.info("Client {} connected", clients.size() + 1);
 
             ClientHandler clientHandler = new ClientHandler(socket, this);
             clients.add(clientHandler);
             new Thread(clientHandler).start();
-            System.out.println("New client connected");
+            logger.info("New client connected");
         }
 
-        if (clients.size() == 2) {
+        if (clients.size() == 2 && !isGameStarted) {
             String startMessage = "The game has started!";
 
             broadcast(startMessage);
             isGameStarted = true;
 
-            UserCommunicationService userCommunicationService = new UserCommunicationService(System.in, System.out);
-            gameSession = userCommunicationService.getGameSessionInfo();
-            gameSession.startGameSession();
+            startGame();
+//            UserCommunicationService userCommunicationService = new UserCommunicationService(System.in, System.out);
+//            gameSession = userCommunicationService.getGameSessionInfo();
+//            gameSession.startGameSession();
         }
 
 //        broadcastMove(new MoveDTO(new Move(
@@ -75,14 +85,14 @@ public class Server {
         return response.toUpperCase();
     }
 
-    public void broadcastMove(MoveDTO move) throws IOException {
+    public void broadcastMove(MoveDTO move) {
         for (ClientHandler client : clients) {
-            String serializedMoveDTO = SerializationService.makeMoveDTOToJson(move);
+            String serializedMoveDTO = SerializationService.convertMoveDTOToJson(move);
             client.sendMove(serializedMoveDTO);
         }
     }
 
-    public synchronized void broadcast(String message) throws IOException { // сообщения обоим игрокам
+    public synchronized void broadcast(String message){ // сообщения обоим игрокам
         for (ClientHandler client : clients) {
             client.sendMessage(message);
         }
@@ -97,7 +107,7 @@ public class Server {
         }
     }
 
-    private synchronized void endGame() throws IOException { //  synchronized (???)
+    private synchronized void endGame() { //  synchronized (???)
         gameStarted = false;
 
         for (ClientHandler client : clients) {
@@ -110,7 +120,7 @@ public class Server {
     }
 
 
-    public Board getBoardState() { 
+    public Board getBoardState() {
         return null;
     }
 
@@ -119,5 +129,71 @@ public class Server {
     }
 
     public void resetGame() {
+    }
+
+    void startGame() {
+        boolean isWhiteTurn = true;
+        for (ClientHandler client : clients) {
+            if (isWhiteTurn) {
+                client.sendMessageToClient("You are white");
+                client.sendMessageToClient("It's your turn!");
+            } else {
+                client.sendMessageToClient("You are black");
+                client.sendMessageToClient("Waiting for opponent's move");
+            }
+
+            isWhiteTurn = !isWhiteTurn;
+        }
+
+        // Игровой цикл
+        Board board = new Board();
+        MoveHistory history = new MoveHistory();
+
+        while (true) {
+            // Ожидание хода от игрока
+            ClientHandler currentPlayer = null;
+            ClientHandler opponentPlayer = null;
+            for (ClientHandler client : clients) {
+                if ((client.getPlayerColor() == Color.WHITE && isWhiteTurn) ||
+                        (client.getPlayerColor() == Color.BLACK && !isWhiteTurn)) {
+                    currentPlayer = client;
+                } else {
+                    opponentPlayer = client;
+                }
+            }
+
+            Move move = currentPlayer.receiveMoveFromClient();
+
+            // Проверка возможности совершения хода
+            if (!board.getPiece(move.startPosition()).canMoveAt(move.endPosition(), board)) {
+                currentPlayer.sendMessageToClient("Invalid move. Please try again.");
+                continue;
+            }
+
+            // Обновление доски и истории
+            board.move(move);
+            history.addMove(move);
+
+            // Проверка на завершение игры
+//            if () {
+//                for (ClientHandler client : clients) {
+//                    client.sendMessageToClient("Game over!");
+//                }
+//                System.exit(0);
+//            }
+
+            // Отправка хода оппоненту
+            String moveJson = objectMapper.toString();
+            opponentPlayer.sendMessageToClient(moveJson);
+
+            // Смена хода
+            isWhiteTurn = !isWhiteTurn;
+            currentPlayer.sendMessageToClient("Waiting for opponent's move");
+            opponentPlayer.sendMessageToClient("It's your turn!");
+        }
+    }
+
+    public void broadcastGameState(GameState gameState) {
+        // Рассылает состояние игры всем клиентам
     }
 }
