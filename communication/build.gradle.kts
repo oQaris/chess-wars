d352292b0e2eb3be38f6f14e69ce74c6725ecf23
pai9maker <rapolovgenrik@gmail.com>
2023-08-18 13:25:51 +0300

    feat: added Converter class for DTO classes

--- communication/build.gradle.kts
+++ communication/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
+    implementation(project(":core"))
+
     implementation ("com.fasterxml.jackson.core:jackson-core:2.15.1")
     implementation ("com.fasterxml.jackson.core:jackson-databind:2.15.1")
     implementation ("com.fasterxml.jackson.core:jackson-annotations:2.15.1")
